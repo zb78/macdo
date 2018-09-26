c10b34222e8e3967280d44a18aa8dd54bd450ce4
cdi314 <cdi314@pltz861.AFPA-PARIS-DI>
2018-09-26 17:19:59 +0200

    Creationd des donnees

--- macdo-ejb/src/java/Entites/Commande.java
+++ macdo-ejb/src/java/Entites/Commande.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;

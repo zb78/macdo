a46f6ae641242eceb4e900e03be934f389e17102
cdi312 <cdi312@pltz845.AFPA-PARIS-DI>
2018-10-01 10:33:49 +0200

    modif macdo-ejb/Beans/CreationDonnees +  macdo-ejb/Entites/Produit et Statut

--- macdo-ejb/src/java/Beans/CreationDonnees.java
+++ macdo-ejb/src/java/Beans/CreationDonnees.java
@@ -90,72 +90,72 @@ public class CreationDonnees implements CreationDonneesLocal {
         /*   // PROBLEME CONCERNANT LES PRIX //
                 ici 4.0F c'est le prix HT du BIG MAC (qui est à 4.40€TTC sur le site)
         */
-//        produit1.getElements().add(element1);
-//        produit1.getElements().add(element2);
-//        produit1.getElements().add(element3);
-//        produit1.getElements().add(element4);
-//        produit1.getElements().add(element5);
-//        produit1.setType(type2);
+        produit1.getElements().add(element1);
+        produit1.getElements().add(element2);
+        produit1.getElements().add(element3);
+        produit1.getElements().add(element4);
+        produit1.getElements().add(element5);
+        produit1.setType(type2);
         Produit produit2 = new Produit("GRANDE FRITE", 2.6F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.", 
-                "image");
-//        produit2.setType(type3);
+                "/WEB-INF/Images_Projet_Team2/GrandeFrite.png");
+        produit2.setType(type3);
         Produit produit3 = new Produit("GRAND COCA-COLA®", 2.6F, statut2, 
                 "50cl\n\n original taste = goût original", 
-                "image");
+                "/WEB-INF/Images_Projet_Team2/GrandCoca.png");
         produit3.getProprietes().add(propriete1);
-//        produit3.getElements().add(element6);
-//        produit3.setType(type4);
+        produit3.getElements().add(element6);
+        produit3.setType(type4);
         Produit produit4 = new Produit("SALADE CHICKEN CAESAR", 7.3F, statut2, 
                 "Salade composée d'un mélange de romaine et de batavia, de 2 spécialités panées au poulet, de tomates-cerises, de copeaux de fromage et de délicieux croûtons saveur ail et fines herbes.\n\nServie avec une sauce Caesar.\n\nChicken = spécialité panée au poulet.", 
-                "image");
-//        produit4.setType(type5);
+                "/WEB-INF/Images_Projet_Team2/saladeCesar.png");
+        produit4.setType(type5);
         produit4.getOptionGratuites().add(optionGratuite0);
         produit4.getOptionGratuites().add(optionGratuite1);
         Produit produit5 = new Produit("EVIAN® (33CL)", 2.3F, statut2, 
                 "EVIAN® (33CL)",
-                "image");
+                "/WEB-INF/Images_Projet_Team2/evian33.png");
         produit5.getProprietes().add(propriete2);
-//        produit5.setType(type4);
+        produit5.setType(type4);
         Produit produit6 = new Produit("MOYENNE FRITE", 2.3F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.",
-                "image");
-//        produit6.setType(type3);
+                "/WEB-INF/Images_Projet_Team2/MoyenneFrite.png");
+        produit6.setType(type3);
         Produit produit7 = new Produit("MOYEN COCA-COLA®", 2.3F, statut2, 
                 "40cl", 
-                "image");  
+                "/WEB-INF/Images_Projet_Team2/MoyenCoca.png");
         produit7.getProprietes().add(propriete3);
-//        produit7.getElements().add(element6);
-//        produit7.setType(type4);
+        produit7.getElements().add(element6);
+        produit7.setType(type4);
         Produit produit8 = new Produit("CHEESEBURGER", 1.8F, statut1, 
                 "Un steak haché, une tranche de cheddar fondu, des cornichons, des oignons, du ketchup et de la moutarde douce dans un pain classique : fondez pour son goût unique et emblématique.\n\nPain spécial, steak haché, fromage fondu, oignon, cornichon, moutarde, ketchup.", 
-                "image");  
-//        produit8.getElements().add(element2);
-//        produit8.getElements().add(element4);
-//        produit8.getElements().add(element5);
-//        produit8.getElements().add(element7);
-//        produit8.getElements().add(element8);
-//        produit8.setType(type2);
+                "/WEB-INF/Images_Projet_Team2/cheeseburger.png");
+        produit8.getElements().add(element2);
+        produit8.getElements().add(element4);
+        produit8.getElements().add(element5);
+        produit8.getElements().add(element7);
+        produit8.getElements().add(element8);
+        produit8.setType(type2);
         Produit produit9 = new Produit("PETITE FRITE", 1.8F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.", 
-                "image");   
+                "/WEB-INF/Images_Projet_Team2/petiteFrite.png");
         produit9.getProprietes().add(propriete4);
-//        produit9.setType(type4);
+        produit9.setType(type4);
         Produit produit10 = new Produit("MINUTE MAID® ORANGE (20CL)", 1.77F, statut2, 
                 "20cl", 
-                "image");
-//        produit10.setType(type3);
+                "/WEB-INF/Images_Projet_Team2/minuteMaid20.png");
+        produit10.setType(type3);
         Produit produit11 = new Produit("P'TITE POMME", 1.36F, statut2, 
                 "Apportez une note fruitée à votre repas. Craquez pour les savoureux quartiers de pommes croquants.", 
-                "image");   
-//        produit11.setType(type6);
+                "/WEB-INF/Images_Projet_Team2/ptitePomme.png");
+        produit11.setType(type6);
         Produit produit12 = new Produit("LIVRE", 2.37F, statut2, 
                 "Place à la lecture dans ton Happy Meal ! En ce moment, 2 livres inédits à collectionner.", 
-                "image");  
-//        produit12.setType(type7);
+                "/WEB-INF/Images_Projet_Team2/livre.png");
+        produit12.setType(type7);
         Produit produit13 = new Produit("SAUCE CAESAR", 0.18F, statut2, 
                 "LA SAUCE CAESAR", 
-                "image");  
+                "/WEB-INF/Images_Projet_Team2/sauceCesar.png");
         produit13.setType(type8);
         
         optionGratuite0.getProduits().add(produit4);
@@ -166,7 +166,7 @@ public class CreationDonnees implements CreationDonneesLocal {
         // !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! 
         Menu menu1 = new Menu("MENU MAXI BEST OF™", 7.4F, 
                 "LE MENU MAXI BEST OF, C'EST...\n- 1 sandwich au choix* - 1 grande portion de frites, ou de Deluxe Potatoes, ou 1 P'tite Salade - 1 bouteille d'Evian 50cl, ou 1 bouteille de Badoit 50cl, ou 1 Minute Maid orange 47cl, ou 1 soda 50cl", 
-                "image");
+                "/WEB-INF/Images_Projet_Team2/MenuBestOf&MenuMaxiBestOf.png");
         /*   // PROBLEME CONCERNANT LES PRIX //
                 ici 7.4F c'est le prix HT du MENU MAXI BEST OF BIG MAC (à 8.10€TTC sur le site)
                 Et les autres 'MENU MAXI BEST OF' sont par exmple à 13.95€TTC pour le signature,
@@ -175,34 +175,33 @@ public class CreationDonnees implements CreationDonneesLocal {
         menu1.setType(type1);
         Menu menu2 = new Menu("MENU SALADE", 7.27F, 
                 "MENU SALADE\nMenu Salade", 
-                "image");
+                "/WEB-INF/Images_Projet_Team2/menuSalade.png");
         menu2.setType(type1);
         Menu menu3 = new Menu("MENU BEST OF™", 7.0F, 
                 "LE MENU BEST OF, C'EST...\n1 sandwich au choix* - 1 portion moyenne de frites, ou de Deluxe Potatoes, ou 1 P'tite Salade - 1 bouteille d'Evian 33cl, ou 1 bouteille de Badoit 33cl, ou 1 Minute Maid orange 30cl, ou 1 soda 40cl.", 
-                "image");
+                "/WEB-INF/Images_Projet_Team2/MenuBestOf&MenuMaxiBestOf.png");
         menu3.setType(type1);
         Menu menu4 = new Menu("MENU HAPPY MEAL", 3.64F, 
                 "HAPPY MEAL\n- un plat au choix (McFish, Croque McDo, Hamburger, Cheeseburger, Chicken McNuggets)\n\n- un accompagnement au choix (Petite Frite, Crousties, Moyenne Deluxe Potatoes, P'tites Tomates)\n\n- une boisson gazeuse 25cl, ou un jus de fruits Bio ou un Minute Maid Orange 20cl, ou un Lipton Ice Tea 25cl, Evian 33cl\n\n- un dessert au choix (P'tite Pomme, Mon Bio à boire, Berlingo' ou le fruit du moment) et aussi choisissez entre un super jouet ou un beau livre.", 
-                "image");
+                "/WEB-INF/Images_Projet_Team2/menuHappyMeal.png");
         menu4.setType(type1);
         
         
-        //LigneDeCommande ligneDeCommande = new LigneDeCommande(QUANTITE, PRIX, TVA, COMMANDE);
         LigneDeCommande ligneDeCommande1 = new LigneDeCommande(1, menu1.getPrix(), tva1.getTaux(), commande1);
         LigneDeCommande ligneDeCommande11 = new LigneDeCommande(1, produit1.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande11.setLigneParent(ligneDeCommande1);
-//        ligneDeCommande11.getElements().add(element1);
-//        ligneDeCommande11.getElements().add(element2);
-//        ligneDeCommande11.getElements().add(element3);
-//        ligneDeCommande11.getElements().add(element4);
-//        ligneDeCommande11.getElements().add(element5);
+        ligneDeCommande11.getElements().add(element1);
+        ligneDeCommande11.getElements().add(element2);
+        ligneDeCommande11.getElements().add(element3);
+        ligneDeCommande11.getElements().add(element4);
+        ligneDeCommande11.getElements().add(element5);
         ligneDeCommande11.setProduit(produit1);
         LigneDeCommande ligneDeCommande12 = new LigneDeCommande(1, produit2.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande12.setLigneParent(ligneDeCommande1);
         ligneDeCommande12.setProduit(produit2);
         LigneDeCommande ligneDeCommande13 = new LigneDeCommande(1, produit3.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande13.setLigneParent(ligneDeCommande1);
-//        ligneDeCommande13.getElements().add(element6);
+        ligneDeCommande13.getElements().add(element6);
         ligneDeCommande13.setProduit(produit3);
         
         LigneDeCommande ligneDeCommande2 = new LigneDeCommande(1, menu2.getPrix(), tva1.getTaux(), commande1);
@@ -226,17 +225,17 @@ public class CreationDonnees implements CreationDonneesLocal {
         ligneDeCommande32.setProduit(produit6);
         LigneDeCommande ligneDeCommande33 = new LigneDeCommande(1, produit7.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande33.setLigneParent(ligneDeCommande3);
-//        ligneDeCommande33.getElements().add(element6);
+        ligneDeCommande33.getElements().add(element6);
         ligneDeCommande33.setProduit(produit7);
         
         LigneDeCommande ligneDeCommande4 = new LigneDeCommande(1, menu4.getPrix(), tva1.getTaux(), commande1);
         LigneDeCommande ligneDeCommande41 = new LigneDeCommande(1, produit1.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande41.setLigneParent(ligneDeCommande4);
-//        ligneDeCommande41.getElements().add(element2);
-//        ligneDeCommande41.getElements().add(element4);
-//        ligneDeCommande41.getElements().add(element5);
-//        ligneDeCommande41.getElements().add(element7);
-//        ligneDeCommande41.getElements().add(element8);
+        ligneDeCommande41.getElements().add(element2);
+        ligneDeCommande41.getElements().add(element4);
+        ligneDeCommande41.getElements().add(element5);
+        ligneDeCommande41.getElements().add(element7);
+        ligneDeCommande41.getElements().add(element8);
         ligneDeCommande41.setProduit(produit8);
         LigneDeCommande ligneDeCommande42 = new LigneDeCommande(1, produit9.getPrix(), tva1.getTaux(), commande1);
         ligneDeCommande42.setLigneParent(ligneDeCommande4);
@@ -254,7 +253,7 @@ public class CreationDonnees implements CreationDonneesLocal {
         em.persist(ligneDeCommande11);
         em.persist(ligneDeCommande12);
         em.persist(ligneDeCommande13);
-        em.persist(ligneDeCommande211);
+//        em.persist(ligneDeCommande211);
         em.persist(ligneDeCommande21);
         em.persist(ligneDeCommande22);
         em.persist(ligneDeCommande31);

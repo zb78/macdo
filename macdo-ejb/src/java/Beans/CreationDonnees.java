799f0a4b64912bd088eca7ef195109c1aa565ae7
cdi312 <cdi312@pltz845.AFPA-PARIS-DI>
2018-10-02 11:57:07 +0200

    modif macdo-war/WEB-INF/home.jsp et menu-gauche.jsp + crea macdo-war/WEB/style.css + modif images

--- macdo-ejb/src/java/Beans/CreationDonnees.java
+++ macdo-ejb/src/java/Beans/CreationDonnees.java
@@ -86,7 +86,7 @@ public class CreationDonnees implements CreationDonneesLocal {
         // !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! 
         Produit produit1 = new Produit("Big Mac", 4.0F, statut1,
                 "Deux steack hachés, du cheddar fondu, 3 buns de pain, des oignons, des cornichons, deux lits de salade et une sauce inimitable...", 
-                "/WEB-INF/Images_Projet_Team2/BigMac.jpg");
+                "./Images/BigMac.jpg");
         /*   // PROBLEME CONCERNANT LES PRIX //
                 ici 4.0F c'est le prix HT du BIG MAC (qui est à 4.40€TTC sur le site)
         */
@@ -98,38 +98,38 @@ public class CreationDonnees implements CreationDonneesLocal {
         produit1.setType(type2);
         Produit produit2 = new Produit("GRANDE FRITE", 2.6F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.", 
-                "/WEB-INF/Images_Projet_Team2/GrandeFrite.png");
+                "./Images/GrandeFrite.png");
         produit2.setType(type3);
         Produit produit3 = new Produit("GRAND COCA-COLA®", 2.6F, statut2, 
                 "50cl\n\n original taste = goût original", 
-                "/WEB-INF/Images_Projet_Team2/GrandCoca.png");
+                "./Images/GrandCoca.png");
         produit3.getProprietes().add(propriete1);
         produit3.getElements().add(element6);
         produit3.setType(type4);
         Produit produit4 = new Produit("SALADE CHICKEN CAESAR", 7.3F, statut2, 
                 "Salade composée d'un mélange de romaine et de batavia, de 2 spécialités panées au poulet, de tomates-cerises, de copeaux de fromage et de délicieux croûtons saveur ail et fines herbes.\n\nServie avec une sauce Caesar.\n\nChicken = spécialité panée au poulet.", 
-                "/WEB-INF/Images_Projet_Team2/saladeCesar.png");
+                "./Images/saladeCesar.png");
         produit4.setType(type5);
         produit4.getOptionGratuites().add(optionGratuite0);
         produit4.getOptionGratuites().add(optionGratuite1);
         Produit produit5 = new Produit("EVIAN® (33CL)", 2.3F, statut2, 
                 "EVIAN® (33CL)",
-                "/WEB-INF/Images_Projet_Team2/evian33.png");
+                "./Images/evian33.png");
         produit5.getProprietes().add(propriete2);
         produit5.setType(type4);
         Produit produit6 = new Produit("MOYENNE FRITE", 2.3F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.",
-                "/WEB-INF/Images_Projet_Team2/MoyenneFrite.png");
+                "./Images/MoyenneFrite.png");
         produit6.setType(type3);
         Produit produit7 = new Produit("MOYEN COCA-COLA®", 2.3F, statut2, 
                 "40cl", 
-                "/WEB-INF/Images_Projet_Team2/MoyenCoca.png");
+                "./Images/MoyenCoca.png");
         produit7.getProprietes().add(propriete3);
         produit7.getElements().add(element6);
         produit7.setType(type4);
         Produit produit8 = new Produit("CHEESEBURGER", 1.8F, statut1, 
                 "Un steak haché, une tranche de cheddar fondu, des cornichons, des oignons, du ketchup et de la moutarde douce dans un pain classique : fondez pour son goût unique et emblématique.\n\nPain spécial, steak haché, fromage fondu, oignon, cornichon, moutarde, ketchup.", 
-                "/WEB-INF/Images_Projet_Team2/cheeseburger.png");
+                "./Images/cheeseburger.png");
         produit8.getElements().add(element2);
         produit8.getElements().add(element4);
         produit8.getElements().add(element5);
@@ -138,24 +138,24 @@ public class CreationDonnees implements CreationDonneesLocal {
         produit8.setType(type2);
         Produit produit9 = new Produit("PETITE FRITE", 1.8F, statut1, 
                 "Ce sont toujours les vedettes des restaurants. Créées à partir de pommes de terre à chair blanche, elles sont aussi longues que croustillantes. Cuites avec de l'huile végétale, elles sont servies avec du ketchup, de la sauce moutarde ou une sauce spéciale.", 
-                "/WEB-INF/Images_Projet_Team2/petiteFrite.png");
+                "./Images/petiteFrite.png");
         produit9.getProprietes().add(propriete4);
         produit9.setType(type4);
         Produit produit10 = new Produit("MINUTE MAID® ORANGE (20CL)", 1.77F, statut2, 
                 "20cl", 
-                "/WEB-INF/Images_Projet_Team2/minuteMaid20.png");
+                "./Images/minuteMaid20.png");
         produit10.setType(type3);
         Produit produit11 = new Produit("P'TITE POMME", 1.36F, statut2, 
                 "Apportez une note fruitée à votre repas. Craquez pour les savoureux quartiers de pommes croquants.", 
-                "/WEB-INF/Images_Projet_Team2/ptitePomme.png");
+                "./Images/ptitePomme.png");
         produit11.setType(type6);
         Produit produit12 = new Produit("LIVRE", 2.37F, statut2, 
                 "Place à la lecture dans ton Happy Meal ! En ce moment, 2 livres inédits à collectionner.", 
-                "/WEB-INF/Images_Projet_Team2/livre.png");
+                "./Images/livre.png");
         produit12.setType(type7);
         Produit produit13 = new Produit("SAUCE CAESAR", 0.18F, statut2, 
                 "LA SAUCE CAESAR", 
-                "/WEB-INF/Images_Projet_Team2/sauceCesar.png");
+                "./Images/sauceCesar.png");
         produit13.setType(type8);
         
         optionGratuite0.getProduits().add(produit4);
@@ -166,7 +166,7 @@ public class CreationDonnees implements CreationDonneesLocal {
         // !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! PRIX PRODUIT A REVOIR !!!!!!!!!!! 
         Menu menu1 = new Menu("MENU MAXI BEST OF™", 7.4F, 
                 "LE MENU MAXI BEST OF, C'EST...\n- 1 sandwich au choix* - 1 grande portion de frites, ou de Deluxe Potatoes, ou 1 P'tite Salade - 1 bouteille d'Evian 50cl, ou 1 bouteille de Badoit 50cl, ou 1 Minute Maid orange 47cl, ou 1 soda 50cl", 
-                "/WEB-INF/Images_Projet_Team2/MenuBestOf&MenuMaxiBestOf.png");
+                "./Images/MenuBestOf&MenuMaxiBestOf.png");
         /*   // PROBLEME CONCERNANT LES PRIX //
                 ici 7.4F c'est le prix HT du MENU MAXI BEST OF BIG MAC (à 8.10€TTC sur le site)
                 Et les autres 'MENU MAXI BEST OF' sont par exmple à 13.95€TTC pour le signature,
@@ -175,15 +175,15 @@ public class CreationDonnees implements CreationDonneesLocal {
         menu1.setType(type1);
         Menu menu2 = new Menu("MENU SALADE", 7.27F, 
                 "MENU SALADE\nMenu Salade", 
-                "/WEB-INF/Images_Projet_Team2/menuSalade.png");
+                "./Images/menuSalade.png");
         menu2.setType(type1);
         Menu menu3 = new Menu("MENU BEST OF™", 7.0F, 
                 "LE MENU BEST OF, C'EST...\n1 sandwich au choix* - 1 portion moyenne de frites, ou de Deluxe Potatoes, ou 1 P'tite Salade - 1 bouteille d'Evian 33cl, ou 1 bouteille de Badoit 33cl, ou 1 Minute Maid orange 30cl, ou 1 soda 40cl.", 
-                "/WEB-INF/Images_Projet_Team2/MenuBestOf&MenuMaxiBestOf.png");
+                "./Images/MenuBestOf&MenuMaxiBestOf.png");
         menu3.setType(type1);
         Menu menu4 = new Menu("MENU HAPPY MEAL", 3.64F, 
                 "HAPPY MEAL\n- un plat au choix (McFish, Croque McDo, Hamburger, Cheeseburger, Chicken McNuggets)\n\n- un accompagnement au choix (Petite Frite, Crousties, Moyenne Deluxe Potatoes, P'tites Tomates)\n\n- une boisson gazeuse 25cl, ou un jus de fruits Bio ou un Minute Maid Orange 20cl, ou un Lipton Ice Tea 25cl, Evian 33cl\n\n- un dessert au choix (P'tite Pomme, Mon Bio à boire, Berlingo' ou le fruit du moment) et aussi choisissez entre un super jouet ou un beau livre.", 
-                "/WEB-INF/Images_Projet_Team2/menuHappyMeal.png");
+                "./Images/menuHappyMeal.png");
         menu4.setType(type1);
         
         
--- macdo-war/web/WEB-INF/home.jsp
+++ macdo-war/web/WEB-INF/home.jsp
@@ -4,44 +4,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="./style.css" />
+        <title>Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <style>
-            /* Remove the navbar's default margin-bottom and rounded borders */ 
-            .navbar {
-                margin-bottom: 0;
-                border-radius: 0;
-            }
 
-            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-            .row.content {height: 450px}
-
-            /* Set gray background color and 100% height */
-            .sidenav {
-                padding-top: 20px;
-                background-color: #f1f1f1;
-                height: 100%;
-            }
-
-            /* Set black background color, white text and some padding */
-            footer {
-                background-color: #555;
-                color: white;
-                padding: 15px;
-            }
-
-            /* On small screens, set height to 'auto' for sidenav and grid */
-            @media screen and (max-width: 767px) {
-                .sidenav {
-                    height: auto;
-                    padding: 15px;
-                }
-                .row.content {height:auto;} 
-            }
-        </style>
     </head>
     <body>
         <div class="container-fluid text-center">    
